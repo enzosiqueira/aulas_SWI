c1259b4ae264295805c8bb1472d6d952d33670a9
Enzo Siqueira <100931687+enzosiqueira@users.noreply.github.com>
2023-03-28 11:04:12 -0300

    Add files via upload

---
+++ Aula 28-03/projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Enzo",16);
+        //p1.InsereNome("Cristiano");
+        //p1.InsereSobreNome("Ronaldo");
+        //p1.InsereIdade(38);
+
+        p1.MostraDados();
+    }
+}
