7e91314e01bdb1e6dcb49a286bdd5b406066e561
Enzo Siqueira <100931687+enzosiqueira@users.noreply.github.com>
2023-04-04 09:37:33 -0300

    Add files via upload

---
+++ ExercicioAlunos/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExercicioAluns;
+
+namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1  = new Alunos();
+        Console.WriteLine("Digite seu nome: ");
+        aluno1.nome = Console.ReadLine();
+
+        Console.WriteLine("***************************************");
+        Console.WriteLine("Digite sua 1º nota: ");
+        aluno1.nota1 =  double.Parse(Console.ReadLine()) ; 
+        Console.WriteLine("Digite sua 2º nota: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+
+        aluno1.menssagem();
+    }
+}
